653790bab6052a89b54d17d8ab39773a39204a38
Arcanae <justin.courdesse@gmail.com>
2017-07-27 14:24:44 +0200

    place boats

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let t1cells = document.querySelectorAll(".td1");
 
-console.log(t1cells);
+function clickCell() {
+    this.style.backgroundColor = 'rgba(255,0,0,0.7)';
+    this.removeEventListener("click", clickCell);
+    placeBoat(this.id);
+}
+
+
+let boats = [];
+let boatsnum = 5;
+
+function placeBoat(id) {
+    boats.push(id);
+    console.log(boats);
+    boatsnum--;
+    document.querySelector("#boatsnum").textContent = 'Bateaux restants : ' + boatsnum;
+    if (boatsnum === 0) {
+        for (let value of t1cells) {
+            value.removeEventListener("click", clickCell);
+        }
+    }
+}
 
 for (let value of t1cells) {
-    value.addEventListener("click", function() {
-        this.style.backgroundColor = 'rgba(255,0,0,0.7)';
-    });
+    value.addEventListener("click", clickCell);
 }
